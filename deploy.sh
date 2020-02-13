97d9b1a262e8af4b5b3f7cec203db457e709646a
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2020-02-13 23:46:37 +0900

    [delete] kustomize installer.

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,5 @@ docker push $IMAGE_URI:$IMAGE_TAG
 export KUBECONFIG=/kubeconfig
 touch KUBECONFIG
 echo $KUBE_CONFIG_DATA | base64 -d > KUBECONFIG
-curl -Lo kustomize \
-    https://github.com/kubernetes-sigs/kustomize/releases/download/v{$KUSTOMIZE_VERSION}/kustomize_${KUSTOMIZE_VERSION}_linux_amd64 \
-    && chmod +x kustomize && mv kustomize /usr/local/bin
 kustomize edit set image $IMAGE_URI=$IMAGE_URI:$IMAGE_TAG
 kubectl apply -f $KUBE_APPLY_DIR
