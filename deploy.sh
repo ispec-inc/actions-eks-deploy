b7a0410b2f6ebf6147e8c97ecb076837a28a85a3
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2020-02-13 23:37:27 +0900

    [add] action.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #/bin/bash
+export IMAGE_URI=$1
+if [ -n "$2" ]; then
+export IMAGE_TAG=$2
+else
 export IMAGE_TAG=$(git log -1 --pretty=%H)
+fi
+export KUBE_CONFIG_DATA=$3
+export KUBE_APPLY_DIR=$4
 docker build . -t $IMAGE_URI:$IMAGE_TAG
 aws ecr get-login --no-include-email | sh
 docker push $IMAGE_URI:$IMAGE_TAG
