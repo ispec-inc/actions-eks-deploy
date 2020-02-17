617f90564759480b391f766dc0061a2539208475
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2020-02-17 18:45:34 +0900

    [add] codes.

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #/bin/bash -l
 echo $*
-exec /bin/bash -ec "export IMAGE_URI=$1"
+export IMAGE_URI=$1
 echo IMAGE URL $IMAGE_URI
-exec /bin/bash -ec "if [ -n "$2" ]; then
+if [ -n "$2" ]; then
 export IMAGE_TAG=$2
 else
 export IMAGE_TAG=$(git log -1 --pretty=%H)
-fi"
+fi
 echo IMAGE TAG $IMAGE_TAG
-exec /bin/bash -ec "export KUBE_CONFIG_DATA=$3"
-exec /bin/bash -ec "export KUBE_APPLY_DIR=$4"
+export KUBE_CONFIG_DATA=$3
+export KUBE_APPLY_DIR=$4
 
 docker build . -t $IMAGE_URI:$IMAGE_TAG
 aws ecr get-login --no-include-email | sh
