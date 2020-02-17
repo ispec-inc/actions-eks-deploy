517f2fe59d14e6533822974dfa29eb5ad5f63bb8
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2020-02-17 16:38:00 +0900

    [fix] deploy.

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,15 @@ export IMAGE_TAG=$(git log -1 --pretty=%H)
 fi"
 exec /bin/bash -ec "export KUBE_CONFIG_DATA=$3"
 exec /bin/bash -ec "export KUBE_APPLY_DIR=$4"
-exec /bin/bash -ec "docker build . -t $IMAGE_URI:$IMAGE_TAG"
-exec /bin/bash -ec "aws ecr get-login --no-include-email | sh"
-exec /bin/bash -ec "docker push $IMAGE_URI:$IMAGE_TAG"
-exec /bin/bash -ec "echo $IMAGE_URI:$IMAGE_TAG"
 
-exec /bin/bash -ec "export KUBECONFIG=/kubeconfig"
-exec /bin/bash -ec "touch KUBECONFIG"
-exec /bin/bash -ec "echo $KUBE_CONFIG_DATA | base64 -d > KUBECONFIG"
-exec /bin/bash -ec "kustomize edit set image $IMAGE_URI=$IMAGE_URI:$IMAGE_TAG"
-exec /bin/bash -ec "kubectl apply -f $KUBE_APPLY_DIR"
+docker build . -t $IMAGE_URI:$IMAGE_TAG
+aws ecr get-login --no-include-email | sh
+docker push $IMAGE_URI:$IMAGE_TAG
+echo $IMAGE_URI:$IMAGE_TAG
+
+export KUBECONFIG=/kubeconfig
+touch KUBECONFIG
+echo $KUBE_CONFIG_DATA | base64 -d > KUBECONFIG
+cd $KUBE_APPLY_DIR
+kustomize edit set image $IMAGE_URI=$IMAGE_URI:$IMAGE_TAG
+kubectl apply -k .
